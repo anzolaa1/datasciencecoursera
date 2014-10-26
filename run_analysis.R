c32dec7deda60239db77caca91515b80546f6e9b
anzolaa1 <anzolaa1@gmail.com>
2014-10-26 13:47:42 -0430

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,15 @@
-## Getting and Cleaning Data (getdata-008)
-## Getting and Cleaning Data Course Project
+## Getting and Cleaning Data (getdata-008) Course Project
 
+# Descarga el archivo de datos
 archivo <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(archivo, "getdata-projectfiles-UCI HAR Dataset.zip", method = "wget", quiet = FALSE)
 unzip(zipfile = "getdata-projectfiles-UCI HAR Dataset.zip", list = FALSE, overwrite = TRUE)
 
+#Instala paquetes requeridos para el procesamiento de los datos
 install.packages("data.table")
 install.packages("reshape2")
 
+#Habilita paquetes requeridos para el procesamiento de los datos
 require("data.table")
 require("reshape2")
 
@@ -42,12 +44,11 @@ names(subject_train) = "subject"
 
 train_data_set <- cbind(as.data.table(subject_train), ytrain, Xtrain)
 
-data = rbind(test_data_set, train_data_set)
+data_set = rbind(test_data_set, train_data_set)
 
 id_labels = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data = melt(data, id = id_labels, measure.vars = data_labels)
-
+data_set_labels = setdiff(colnames(data_set), id_labels)
+melt_data = melt(data_set, id = id_labels, measure.vars = data_set_labels)
 
 data_limpia = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
